adcfb77d2a789a24171902972c2eb391f5786343
Cenyo <cenyo.medewou@gmail.com>
2020-10-14 23:22:35 +0200

    adding color detection

--- src/main/java/com/cenyo/frame/controllers/ImageController.java
+++ src/main/java/com/cenyo/frame/controllers/ImageController.java
@@ -1,15 +1,12 @@
 package com.cenyo.frame.controllers;
 
 import com.cenyo.frame.services.FrameFileService;
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
 
 
 @RestController
@@ -22,8 +19,6 @@ public class ImageController {
 
     @RequestMapping("/current")
     public String getAllFolders() throws IOException {
-
-        byte[] fileContent = FileUtils.readFileToByteArray(new File(frameFileService.getUrl()));
-        return Base64.getEncoder().encodeToString(fileContent);
+        return frameFileService.getFile();
     }
 }
---
+++ src/main/java/com/cenyo/frame/utils/ImageDominantColor.java
@@ -0,0 +1,70 @@
+package com.cenyo.frame.utils;
+
+/*https://gist.github.com/jittagornp/6c7fcdab388fe4863c34*/
+
+import java.awt.image.BufferedImage;
+import java.util.*;
+
+public class ImageDominantColor {
+
+    public static List<String> getHexColor(BufferedImage image) {
+        Map<Integer, Integer> colorMap = new HashMap<>();
+        int height = image.getHeight();
+        int width = image.getWidth();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rgb = image.getRGB(i, j);
+                if (!isGray(getRGBArr(rgb))) {
+                    Integer counter = colorMap.get(rgb);
+                    if (counter == null) {
+                        counter = 0;
+                    }
+                    colorMap.put(rgb, ++counter);
+                }
+            }
+        }
+
+        return getMostCommonColor(colorMap);
+    }
+
+    private static List<String> getMostCommonColor(Map<Integer, Integer> map) {
+        List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
+
+        Collections.sort(list, (Map.Entry<Integer, Integer> obj1, Map.Entry<Integer, Integer> obj2)
+                -> ((Comparable) obj1.getValue()).compareTo(obj2.getValue()));
+
+        List<String> result = new ArrayList<>();
+        list.forEach(l -> result.add(hetHexString(l)));
+        return result;
+    }
+
+    private static String hetHexString(Map.Entry<Integer, Integer> entry) {
+        int[] rgb = getRGBArr(entry.getKey());
+
+        return "#" + Integer.toHexString(rgb[0])
+                + Integer.toHexString(rgb[1])
+                + Integer.toHexString(rgb[2]);
+    }
+
+    private static int[] getRGBArr(int pixel) {
+        int alpha = (pixel >> 24) & 0xff;
+        int red = (pixel >> 16) & 0xff;
+        int green = (pixel >> 8) & 0xff;
+        int blue = (pixel) & 0xff;
+
+        return new int[]{red, green, blue, alpha};
+    }
+
+    private static boolean isGray(int[] rgbArr) {
+        int rgDiff = rgbArr[0] - rgbArr[1];
+        int rbDiff = rgbArr[0] - rgbArr[2];
+        int tolerance = 10;
+        if (rgDiff > tolerance || rgDiff < -tolerance) {
+            if (rbDiff > tolerance || rbDiff < -tolerance) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
